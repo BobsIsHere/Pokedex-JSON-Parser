a5a2c97b633ff335cb9bb19389a8a2b31ddb2fbc
BobsIsHere <maritte.kindt@student.howest.be>
2024-10-28 15:07:59 +0100

    Fetching and linking nlohmann library
    Configure CMake to fetch and link nlohmann JSON library + Small test in main.cpp

--- Pokedex/Pokedex.cpp
+++ Pokedex/Pokedex.cpp
@@ -1,6 +1,28 @@
-﻿#include "Pokedex.h"
+﻿#include <nlohmann/json.hpp>
+#include <string>
+#include "Pokedex.h"
+
+void NlohmannParser();
+void RapidJsonParser();
 
 int main()
 {
+	// Parse the JSON file using Nlohmann JSON
+	NlohmannParser();
+
 	return 0;
+}
+
+void NlohmannParser()
+{
+	std::string jsonString = R"({"testKey": "testValue"})";
+
+	nlohmann::json parsedJson{ nlohmann::json::parse(jsonString) };
+
+	std::cout << "testKey: " << parsedJson["testKey"] << std::endl;
+}
+
+void RapidJsonParser()
+{
+
 }
