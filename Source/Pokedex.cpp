e4223ae80ba01616433b36d7b6cecdf8ed07c89c
BobsIsHere <maritte.kindt@student.howest.be>
2024-11-05 22:42:48 +0100

    Changed folder name + Printing UTF-8 characters

--- Source/Pokedex.cpp
+++ Source/Pokedex.cpp
@@ -1,10 +1,14 @@
 ﻿#include <string>
 #include <vector>
+#include <windows.h>
 #include "Pokedex.h"
 #include "JsonParser.h"
 
 int main()
 {
+	// Set console output to UTF-8 so we can print Unicode characters
+	SetConsoleOutputCP(CP_UTF8); 
+
 	JsonParser parser;
 	std::vector<PokemonData> pokemonList = parser.ParsePokemonList("Content/pokedex.json");
 	parser.PrintPokemonList(pokemonList); 
